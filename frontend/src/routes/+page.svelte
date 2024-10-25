29611453ae697a097631feee19eb271e5d55d03b
Mikkel Martinus Fahnøe <mikkelmf@h333.aau1x-2.klient.sydhavn2.site.aau.dk>
2024-10-25 11:16:40 +0200

    Mit forste push

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from 'svelte';
+    let message = 'Loading...';
+    onMount(async () => {
+        try {
+            const response = await fetch('http://localhost:8080/api/hello');
+            const data = await response.text();
+            message = data;
+        } catch (error) {
+            message = 'Error loading message';
+            console.error('Error:', error);
+        }
+    });
+</script>
+<h1>Hello {message}</h1>
---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+// count up to 10 and display while coutning
+let count = $state(0);
+
+</script>
+<p>This is the about page.</p>
+<p>The count is {count}</p>
+<button onclick={count += 1}>Click me</button>
