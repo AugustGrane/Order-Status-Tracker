1395e5e9336a3f09450c53044ad64d7436a36fac
magnusew <magnuswich@live.dk>
2024-10-28 10:05:10 +0100

    added /track route with order search form and /track/{param} ssubpage for input

---
+++ frontend/src/routes/track/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';  // Import goto function
+    let inputText = '';
+    let errorMessage = '';
+
+    function handleSubmit(event:any) {
+        event.preventDefault();
+
+        // Post to database here and check if order exists
+        let isValid:boolean = true;
+
+        if (isValid) {
+            errorMessage = '';
+            goto(`/track/${inputText.trim()}`);
+        } else {
+            errorMessage = 'Order number not found';
+        }
+    }
+</script>
+
+<form on:submit={handleSubmit}>
+    <label for="inputText">Enter Text:</label>
+    <input
+            id="inputText"
+            type="text"
+            bind:value={inputText}
+            placeholder="Type only numbers here"
+            required
+    />
+    <button type="submit">Submit</button>
+
+    {#if errorMessage}
+        <p style="color: red;">{errorMessage}</p>
+    {/if}
+</form>
+
+
---
+++ frontend/src/routes/track/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores';
+
+    // Extract the 'id' parameter from the URL
+    $: id = $page.params.id;
+
+</script>
+
+<p>Hello: {id}</p>
