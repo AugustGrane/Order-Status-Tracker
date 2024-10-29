39be33c627d027e29c098890332a0086239f92e7
Kevin <HY36KK@student.aau.dk>
2024-10-29 13:10:54 +0100

    Item, Line and Step Component
    Divided Items into three components for use in connection with the database

---
+++ frontend/src/routes/track/[id]/components/LineComponent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let done:boolean = false;
+</script>
+
+{#if done}
+    <svg xmlns="http://www.w3.org/2000/svg" width="74" height="2" viewBox="0 0 74 2" fill="none">
+        <path d="M0.727264 1.5L73.4545 1.50001" stroke="#828282" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+{:else}
+    <svg xmlns="http://www.w3.org/2000/svg" width="73" height="2" viewBox="0 0 73 2" fill="none">
+        <path d="M0.181808 1.5L72.9091 1.50001" stroke="#ABABAB" stroke-width="2" stroke-linecap="round"/>
+    </svg>
+{/if}
