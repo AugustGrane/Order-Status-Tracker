61ef777cb1fca278b4d9b639ab31fa2c33cfb7c1
magnusew <magnuswich@live.dk>
2024-10-30 11:06:44 +0100

    Created service class to handle logic and interaction with database

--- backend/src/main/java/gruppe2/backend/service/webhook/WebhookPayload.java
+++ backend/src/main/java/gruppe2/backend/service/webhook/WebhookPayload.java
@@ -1,4 +1,4 @@
-package gruppe2.backend.controller.webhook;
+package gruppe2.backend.service.webhook;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
