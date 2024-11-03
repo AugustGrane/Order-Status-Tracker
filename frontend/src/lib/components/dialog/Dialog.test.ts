b4f9e781f3f6ec9070b580d9151aa3f7cb080087
Fahnoee <mikkel120202@hotmail.com>
2024-11-04 00:00:15 +0100

    added github frontend testing

---
+++ frontend/src/lib/components/dialog/Dialog.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import Dialog from './Dialog.svelte';
+
+describe('Dialog', () => {
+    it('component exists', () => {
+        expect(Dialog).toBeTruthy();
+    });
+});
